e04aae5d247d2809e5cf00e2fd512da1aa6b1177
mbc2579 <mbc2579@naver.com>
2025-03-12 19:08:03 +0900

    파일명 변경

---
+++ DataStructure/src/main/java/queue/MyLinkedQueue.java
@@ -0,0 +1,17 @@
+package queue;
+
+public class MyLinkedQueue<T> implements IQueue<T> {
+    private class Node {
+        T data;
+        Node next;
+
+        Node(T data) {
+            this.data = data;
+        }
+
+        Node(T data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
